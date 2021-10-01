65b03d55481bb8eb747ff8e384f9e72d8a4b4967
AlhawariAlharthi <alhawarial7arthy@gmail.com>
2021-10-01 13:09:12 -0500

    first commet
    exercise 5

---
+++ batch-process-Alharthi.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+for f in "$1"/*; do
+  if [ -f "$f" ]; then
+    SIZE="$(du -sh "${f}" | cut -f1)"
+    number="$(wc -w "${f}"| cut -d' ' -f1)"
+    
+    echo "Processing $f file..."
+    echo "File size: $SIZE"
+    echo "Word count: $number"
+  fi
+done
+
